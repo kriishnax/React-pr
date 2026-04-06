cab97699f1eb83e7f133b13b9e016c4eae265d84
Krishna <krishnasusari@gmail.com>
2026-04-06 18:10:47 +0530

    adding new folderrs

---
+++ tailwind/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 '><h1 >App</h1></div>
+  )
+}
+
+export default App
---
+++ ui-project/src/components/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-gray-900 text-white'>
+      Section 1
+    </div>
+  )
+}
+
+export default Section1
